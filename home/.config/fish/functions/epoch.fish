349b4eb011613233786abf89d4a535c8440cd9d7
Matt Godbolt <matthew.godbolt@aquatic.com>
2020-03-18 11:26:19 -0500

    Update epoch

--- home/.config/fish/functions/epoch.fish
+++ home/.config/fish/functions/epoch.fish
@@ -1,4 +1,7 @@
-# Defined in - @ line 2
 function epoch --description 'Given a number of nanos since epoch, output a UNIX UTC timestamp' --argument epoch_time
+  if echo $argv | grep -v '[:-]' >/dev/null
 	date -u -d @(math "$argv[1] / 1000000000")
+  else
+    date -u -d "$argv" '+%s'000000000
+  end
 end
